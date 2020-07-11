704ecea4ab915c143f660715f389cec0771cca8a
DTR <t.terry.rice@gmail.com>
2020-07-11 16:16:31 +0300

    2.1.1 (add scripts)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const consultationForm = document.querySelector('.consultation__form');
+const contactForm = document.querySelector('.contact__form');
+
+function clean(evnt) {
+  evnt.preventDefault();
+  document.location.href = '/';
+}
+
+consultationForm.addEventListener('submit', clean);
+contactForm.addEventListener('submit', clean);
